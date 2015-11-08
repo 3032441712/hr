356d50d5649d12cd03889d4a9477aa5135801a79
3032441712 <3032441712@qq.com>
2015-11-08 09:39:51 +0800

    TODO:欢迎页面添加系统公告 1 欢迎页面添加系统公告

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -5,7 +5,7 @@ $this->title = '欢迎使用人力资源管理系统';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
+    <div class="jumbotron" style="display: none;">
         <h1>欢迎登录人力资源管理系统</h1>
         <p class="lead"></p>
         <p></p>
@@ -18,6 +18,23 @@ $this->title = '欢迎使用人力资源管理系统';
                 <p></p>
                 <p></p>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4" style="width: 100%;">
+                <!-- Info box -->
+                <div class="box box-solid box-info">
+                    <div class="box-header">
+                        <h3 class="box-title">最新公告</h3>
+                        <div class="box-tools pull-right">
+                            <button class="btn btn-info btn-sm" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                            <button class="btn btn-info btn-sm" style="display: none;" data-widget="remove"><i class="fa fa-times"></i></button>
+                        </div>
+                    </div>
+                    <div class="box-body">
+                        最近天气越来越冷了,大家要多添加衣物,以免感冒.
+                    </div><!-- /.box-body -->
+                </div><!-- /.box -->
+            </div><!-- /.col -->
         </div>
     </div>
 </div>
