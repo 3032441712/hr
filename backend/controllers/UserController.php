476eb0cfcd0ecb05a374133345b658b93d69adb0
zhaoyan <3032441712@qq.com>
2015-11-09 16:03:01 +0800

    TODO:禁用员工信息的删除功能 1 禁用员工信息的删除功能

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -122,14 +122,16 @@ class UserController extends CController
     }
 
     /**
-     * Deletes an existing User model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * 删除员工信息,目前不支持员工信息的删除.
+     * 本功能目前属于禁用状态.
+     * 
      * @param integer $id
+     * 
      * @return mixed
      */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
+//         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
     }
