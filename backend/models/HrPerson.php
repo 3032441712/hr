b0e1d01984cb8c0a2021f794d3cf23df8b610b21
zhaoyan <3032441712@qq.com>
2015-11-09 14:05:54 +0800

    TODO:修正个人信息显示不完整 1 修正个人信息显示不完整

--- backend/models/HrPerson.php
+++ backend/models/HrPerson.php
@@ -48,6 +48,22 @@ class HrPerson extends DateTimeModel
     public $password;
 
     public $repassword;
+    
+    private $_statusLabel;
+    
+    private $_maritalStatusLabel;
+    
+    private $_sexLabel;
+    
+    private $_workingStatusLabel;
+    
+    private $_jobTypeLabel;
+    
+    private $_jobStationLabel;
+    
+    private $_jobLevelLabel;
+    
+    private $_attendanceTypeLabel;
 
     /**
      * @inheritdoc
@@ -92,7 +108,87 @@ class HrPerson extends DateTimeModel
             ]
         ];
     }
-
+    
+    public function getStatusLabel()
+    {
+        if ($this->_statusLabel == null) {
+            $statusLabel = User::getArrayStatus();
+            $this->_statusLabel = $statusLabel[$this->status];
+        }
+        
+        return $this->_statusLabel;
+    }
+    
+    public function getMaritalStatusLabel()
+    {
+        if ($this->_maritalStatusLabel == null) {
+            $maritalStatusLabel = User::getArrayMaritalStatus();
+            $this->_maritalStatusLabel = $maritalStatusLabel[$this->marital_status];
+        }
+        
+        return $this->_maritalStatusLabel;
+    }
+    
+    public function getSexLabel()
+    {
+        if ($this->_sexLabel == null) {
+            $sexLabel = User::getArraySex();
+            $this->_sexLabel = $sexLabel[$this->sex];
+        }
+        
+        return $this->_sexLabel;
+    }
+    
+    public function getWorkingStatusLabel()
+    {
+        if ($this->_workingStatusLabel == null) {
+            $workingStatusLabel = User::getArrayWorkingStatus();
+            $this->_workingStatusLabel = $workingStatusLabel[$this->working_status];
+        }
+        
+        return $this->_workingStatusLabel;
+    }
+    
+    public function getJobTypeLabel()
+    {
+        if ($this->_jobTypeLabel == null) {
+            $jobTypeLabel = User::getArrayJobType();
+            $this->_jobTypeLabel = $jobTypeLabel[$this->job_type];
+        }
+        
+        return $this->_jobTypeLabel;
+    }
+    
+    public function getJobStationLabel()
+    {
+        if ($this->_jobStationLabel == null) {
+            $jobStationLabel = User::getArrayJobStation($this->dept_id);
+            $this->_jobStationLabel = $jobStationLabel[$this->job_station];
+        }
+        
+        return $this->_jobStationLabel;
+    }
+    
+    public function getJobLevelLabel()
+    {
+        if ($this->_jobLevelLabel == null) {
+            $jobLevelLabel = User::getArrayJobLevel();
+            $this->_jobLevelLabel = $jobLevelLabel[$this->job_level];
+        }
+        
+        return $this->_jobLevelLabel;
+    }
+    
+    public function getAttendanceTypeLabel()
+    {
+        if ($this->_attendanceTypeLabel == null) {
+            $attendanceTypeLabel = User::getArrayAttendanceType();
+            $this->_attendanceTypeLabel = $attendanceTypeLabel[$this->attendance_type];
+        }
+        
+        return $this->_attendanceTypeLabel;
+    }
+    
     /**
      * @inheritdoc
      */
--- backend/views/person/view.php
+++ backend/views/person/view.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use app\models\HrRegion;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\HrPerson */
@@ -28,14 +29,21 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'real_name',
                 'label' => '姓名'
             ],
-            'sex',
+            [
+                'attribute' => 'sex',
+                'value' => $model->sexLabel
+            ],
             'birthday',
-            'marital_status',
+            [
+                'attribute' => 'marital_status',
+                'value' => $model->maritalStatusLabel
+            ],
             'last_login',
-            'province',
-            'city',
-            'district',
-            'address',
+            [
+                'label' => '户籍所在地',
+                'attribute' => 'address',
+                'value' => HrRegion::getAreaTitle([$model->province,$model->city,$model->district]).' '.$model->address
+            ],
             'zipcode',
             'qq',
             'office_phone',
@@ -43,23 +51,41 @@ $this->params['breadcrumbs'][] = $this->title;
             'mobile_phone',
             'entry_time',
             'salary_time',
-            'working_status',
-            'job_type',
-            'job_station',
-            'job_level',
-            'attendance_type',
+            [
+                'attribute' => 'working_status',
+                'value' => $model->workingStatusLabel
+            ],
+            [
+                'attribute' => 'job_type',
+                'value' => $model->jobTypeLabel
+            ],
+            [
+                'attribute' => 'job_station',
+                'value' => $model->jobStationLabel
+            ],
+            [
+                'attribute' => 'job_level',
+                'value' => $model->jobLevelLabel
+            ],
+            [
+                'attribute' => 'attendance_type',
+                'value' => $model->attendanceTypeLabel
+            ],
             'email:email',
             [
                 'attribute' => 'status',
-                'label' => '状态'
+                'label' => '状态',
+                'value' => $model->statusLabel
             ],
             [
                 'attribute' => 'created_at',
-                'label' => '创建时间'
+                'label' => '创建时间',
+                'value' => date('Y-m-d H:i:s', $model->created_at)
             ],
             [
                 'attribute' => 'updated_at',
-                'label' => '更新时间'
+                'label' => '更新时间',
+                'value' => date('Y-m-d H:i:s', $model->updated_at)
             ],
         ],
     ]) ?>
